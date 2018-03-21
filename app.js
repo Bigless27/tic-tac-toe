d4ef3ced80d664548466152571d8aaf65eae11a4
Bigless27 <lessonryan@gmail.com>
2018-03-20 22:29:52 -0700

    add check for winner method

--- app.js
+++ app.js
@@ -18,19 +18,54 @@ players = {
 }
 
 var currentTurn = 1;
+var movesMade = 0;
 
 var sqr = $('.square');
 
 
 sqr.on('click', (e) => {
+    movesMade++;
     //this is keeping track of whose turn it is
     if (currentTurn % 2 === 1) {
         event.target.innerHTML = players[currentTurn];
         event.target.style.color = "red";
+        if (checkForWinner()) {
+            console.log(players[currentTurn] + " Wins!");
+        }
         currentTurn++;
     } else {
         event.target.innerHTML = players[currentTurn];
         event.target.style.color = "green";
+        if (checkForWinner()) {
+            console.log(players[currentTurn] + " Wins!");
+        }
         currentTurn--;
     }
-})
+});
+
+function checkForWinner() {
+    //need at least four moves to check for a winner
+    if (movesMade > 4) {
+        var sqr = $('.square');
+        //research why we need call here!
+        var moves = Array.prototype.slice.call($(".square"));
+        var results = moves.map(function(square) { return square.innerHTML; });
+        var winningCombos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        return winningCombos.find(function(combo) {
+            if (results[combo[0]] !== "" && results[combo[1]] !== "" && results[combo[2]] !== "" && results[combo[0]] === results[combo[1]] && results[combo[1]] === results[combo[2]]) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+    }
+}
